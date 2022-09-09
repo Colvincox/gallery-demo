7df74496fc5c558ebbf9c19fa1c2b88d9d003360
colvin cox <calvincoxjr@gmail.com>
2022-09-09 02:09:01 -0400

    changes to gallery-item unit test

--- src/app/components/gallery-item/gallery-item.component.spec.ts
+++ src/app/components/gallery-item/gallery-item.component.spec.ts
@@ -15,7 +15,7 @@ describe('GalleryItemComponent', () => {
     fixture = TestBed.createComponent(GalleryItemComponent);
     component = fixture.componentInstance;
     let img: Img = {
-      albumId: 1, 
+      albumId: 1,
       id: 1,
       title: 'accusamus beatae ad facilis cum similique qui sunt',
       url: 'https://via.placeholder.com/600/92c952',
@@ -27,5 +27,16 @@ describe('GalleryItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('shouldset correct img values', () => {
+    expect(component.img.albumId).toEqual(1);
+    expect(component.img.id).toEqual(1);
+    expect(component.img.title).toEqual(
+      'accusamus beatae ad facilis cum similique qui sunt'
+    );
+    expect(component.img.url).toEqual('https://via.placeholder.com/600/92c952');
+    expect(component.img.thumbnailUrl).toEqual(
+      'https://via.placeholder.com/150/92c952'
+    );
   });
 });
--- src/app/components/gallery-item/gallery-item.component.ts
+++ src/app/components/gallery-item/gallery-item.component.ts
@@ -4,17 +4,16 @@ import { Img } from '../../Img';
 @Component({
   selector: 'app-gallery-item',
   templateUrl: './gallery-item.component.html',
-  styleUrls: ['./gallery-item.component.css']
+  styleUrls: ['./gallery-item.component.css'],
 })
 export class GalleryItemComponent implements OnInit {
   @Input() img: Img;
-  @Output() sendViewLargeImageEvent: EventEmitter<Img> = new EventEmitter(); 
+  @Output() sendViewLargeImageEvent: EventEmitter<Img> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-  onViewLargeImageClick(img: Img){
+  ngOnInit(): void {}
+  onViewLargeImageClick(img: Img) {
     this.sendViewLargeImageEvent.emit(img);
   }
 }
